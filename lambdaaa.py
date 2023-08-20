33af6cf92a27572ee3b256fb8881d9f9523c227d
Nasim <nasim.rjb@gmail.com>
2023-08-20 15:36:59 +0330

    Create lambdaaa.py

---
+++ lambdaaa.py
@@ -0,0 +1,19 @@
+# def cubes(x): return x*x*x
+
+
+# print(cubes(3))
+
+# lst = [1, 2, 3, 4, 5]
+
+# dec_list = list(map(lambda x: x-1, lst))
+# print(dec_list)
+
+
+# def remove_negatives(lst):
+#     print(list(filter(lambda x: x >= 0, lst)))
+
+
+# remove_negatives([1, -3, 4, 5, -9, 0, -7])
+def extremes(l):
+    m = sorted(l)
+    return (m[0], m[-1])
