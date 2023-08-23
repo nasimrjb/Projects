5e58940ac5cf073a83d7172881a3c1f9e5af9db5
Nasim <nasim.rjb@gmail.com>
2023-08-23 21:42:06 +0330

    Update colors.py

--- colors.py
+++ colors.py
@@ -1,4 +1,11 @@
 from colorama import init
 from termcolor import colored
-# help(termcolor)
-print(colored('Hello, World!', 'red', 'on_magenta', ['bold', 'blink']))
+import pyfiglet
+# # help(termcolor)
+# print(colored('Hello, World!', 'red', 'on_magenta', ['blink']))
+
+colorr = input("what color do you want? ")
+text = input("what do you wanna print? ")
+
+msg = pyfiglet.figlet_format(text)
+print(colored(msg, color="green"))
