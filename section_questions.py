d7ab536e305182ed5e6086e23a449922a87da09f
Nasim <nasim.rjb@gmail.com>
2023-08-31 16:55:49 +0330

    j

--- section_questions.py
+++ section_questions.py
@@ -1,20 +1,25 @@
-def reverse_string(string):
-    return string[::-1]
+# def reverse_string(string):
+#     return string[::-1]
 
 
-print(reverse_string("Nasimoo"))
-
-####################################################
-
-
-def is_list(lst):
-    for l in lst:
-        if type(l) != list:
-            return False
-    return True
-
-
-print(is_list([[1], [1, 2], [], [2, 3, 4]]))
-
+# print(reverse_string("Nasimoo"))
 
 ####################################################
+
+
+# def is_list(lst):
+#     for l in lst:
+#         if type(l) != list:
+#             return False
+#     return True
+
+
+# print(is_list([[1], [1, 2], [], [2, 3, 4]]))
+
+
+####################################################
+def remove_every_oder (lst):
+    l =[]
+    for i in range (len(lst)-1):
+        if i%2 == 0:
+            
