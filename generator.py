8c36471f157c353a22c8f48dcac06ef3d107b3b0
Nasim <nasim.rjb@gmail.com>
2023-09-15 00:24:02 +0330

    Update generator.py

--- generator.py
+++ generator.py
@@ -56,3 +56,38 @@
 
 
 ######################################################################
+def get_multiples(num=1, count=10):
+    sum = num
+    for i in range(count):
+        yield sum
+        sum += num
+
+
+evens = get_multiples(2, 3)
+print(next(evens))  # 2
+print(next(evens))  # 2
+print(next(evens))  # 2
+
+
+default_multiples = get_multiples()
+print(list(default_multiples))  # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+#####################################################################
+def get_unlimited_multiples(num=1):
+    sum = num
+    while True:
+        yield sum
+        sum += num
+
+
+sevens = get_unlimited_multiples(7)
+
+
+a = [next(sevens) for i in range(15)]
+print(a)
+# [7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98, 105]
+
+ones = get_unlimited_multiples()
+print([next(ones) for i in range(20)])
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
