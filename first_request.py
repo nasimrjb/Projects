0917dd0297c2e984dd58492bc1601e60dd5c6c86
Nasim <nasim.rjb@gmail.com>
2023-09-19 21:40:25 +0330

    Update first_request.py

--- first_request.py
+++ first_request.py
@@ -7,9 +7,18 @@ import requests
 
 
 #############################################################################
-url = "https://icanhazdadjoke.com/"
-#response = requests.get(url, headers={"Accept": "text/plain"})
-response = requests.get(url, headers={"Accept": "application/json"})
-#print(response.text)
-data = response.json() #python dictionary
-print(data['joke'])
+# url = "https://icanhazdadjoke.com/"
+# #response = requests.get(url, headers={"Accept": "text/plain"})
+# response = requests.get(url, headers={"Accept": "application/json"})
+# #print(response.text)
+# data = response.json() #python dictionary
+# print(data['joke'])
+
+
+#############################################################################
+
+url = "https://icanhazdadjoke.com/search"
+response = requests.get(url, headers={
+                        "Accept": "application/json"}, params={"term": "cat", "limit": 1})
+data = response.json()
+print(data['results'][['joke']])
