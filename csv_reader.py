d7011b0e61cddbc55ad45244b5b3db4a604baa7d
Nasim <nasim.rjb@gmail.com>
2023-09-28 15:38:49 +0330

    Update csv_reader.py

--- csv_reader.py
+++ csv_reader.py
@@ -1,5 +1,13 @@
-from csv import reader
-with open("fighters.csv") as file:
-    csv_reader = reader(file)
-    for row in csv_reader:
-        print(row)
+from csv import writer
+
+
+def add_user(first, last):
+    with open("users.csv.txt", 'a') as file:
+        csv_writer = writer(file)
+        csv_writer.writerow([first, last])
+
+
+add_user("Nasim", "Rajabi")
+add_user("Abbas", "Rajabi")
+add_user("Mahboobeh", "Rajabi")
+##############################################################
