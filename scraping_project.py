726a2dd9a8d773e048fa9ac789d6fda7077cceed
Nasim <nasim.rjb@gmail.com>
2023-10-07 17:42:02 +0330

    Update scraping_project.py

--- scraping_project.py
+++ scraping_project.py
@@ -1,17 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from random import choice
+lnk = "page/1"
 
-http = "https://quotes.toscrape.com/"
-response = requests.get(http)
-soup = BeautifulSoup(response.text, "html.parser")
+while lnk:
+    lnk_short = "https://quotes.toscrape.com/"
+    http = f"{lnk_short}{lnk}"
+    response = requests.get(http)
+    soup = BeautifulSoup(response.text, "html.parser")
 
-lst = []
+    lst = []
+    quotes = soup.find_all(class_="quote")
+    for quote in quotes:
+        lst.append({"text": quote.find(class_="text").get_text(),
+                    "author": quote.find(itemprop="author").get_text(),
+                    "link": quote.find("a")["href"]})
 
-quotes = soup.find_all(class_="quote")
-for quote in quotes:
-    lst.append({"text": quote.find(class_="text").get_text(),
-               "author": quote.find(itemprop="author").get_text(),
-                "link": quote.find("a").attrs})
+    if soup.find(class_="next"):
+        lnk = soup.find(class_="next").find("a")["href"]
+    else:
+        None
+
 
 print(lst)
