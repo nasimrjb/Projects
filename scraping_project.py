d0b10f8c549a09eb6ca7d05f58cd022e9d47b254
Nasim <nasim.rjb@gmail.com>
2023-10-07 15:28:19 +0330

    Update scraping_project.py

--- scraping_project.py
+++ scraping_project.py
@@ -2,8 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-http = "https://www.rithmschool.com/blog/"
+http = "https://quotes.toscrape.com/"
 response = requests.get(http)
 soup = BeautifulSoup(response.text, "html.parser")
-articles = soup.find_all("article")
-print(articles)
+
+lst = []
+
+quotes = soup.find_all(class_="quote")
+for quote in quotes:
+    lst.append({"text": quote.find(class_="text").get_text(),
+               "author": quote.find(itemprop="author").get_text(),
+                "link": quote.find("a").attrs})
+
+print(lst)
