df745d2e80840337b05d6ca8323d17dbed90caa7
Nasim <nasim.rjb@gmail.com>
2023-10-08 12:43:45 +0330

    Update books_toscrape.py

--- books_toscrape.py
+++ books_toscrape.py
@@ -1,7 +1,16 @@
-import requests
-from bs4 import BeautifulSoup
-import csv
+import scrapy
 
-http = "https://books.toscrape.com//"
-response = requests.get(http)
-soup = BeautifulSoup(response.text, "html.parser")
+
+class BookSpider(scrapy.Spider):
+    name = 'bookspider'
+    start_urls = ['https://books.toscrape.com']
+
+    def parse(self, response):
+        for article in response.css('article.product_pod'):
+            yield {
+                'price': article.css(".price_color::text").extract_first(),
+                'title': article.css("h3 > a::attr(title)").extract_first()
+            }
+            next = response.css('.next > a:attr(href)').extract_first()
+            if next:
+                yield response.follow(next, self.parse)
