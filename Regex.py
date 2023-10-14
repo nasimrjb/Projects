ca6040ca5d417649da16404fbbe10a36df71b5f5
Nasim <nasim.rjb@gmail.com>
2023-10-14 19:02:38 +0330

    Update Regex.py

--- Regex.py
+++ Regex.py
@@ -1,3 +1,34 @@
 import re
-pattern = re.compile(r'\d{3} \d{3}-\d{4}')
-res = pattern.search('call me at 415 555-4242!')
+# pattern = re.compile(r'\d{3} \d{3}-\d{4}')
+# res = pattern.search('call me at 415 555-4242!')
+# print(res.group())
+
+##########################################################
+# def is_valid_time(str):
+#     pattern = re.compile(r'^\d\d?:\d\d$')
+#     if pattern.search(str):
+#         return True
+#     else:
+#         return False
+
+
+# print(is_valid_time("44:55"))
+# print(is_valid_time("12:55"))
+# print(is_valid_time("4e4:55"))
+# print(is_valid_time("44:550"))
+# print(is_valid_time("44:sdj55"))
+
+
+#########################################################
+
+
+# def parse_bytes(str):
+#     pattern = re.compile(r'\b[10]{8}\b')
+#     x = pattern.findall(str)
+#     return x
+
+
+# print(parse_bytes('10101101  11011111'))
+
+
+##########################################################
