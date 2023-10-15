85a121b7d53341ea0f70c77e867b87f873671540
Nasim <nasim.rjb@gmail.com>
2023-10-15 20:14:02 +0330

    Update challenge_section.py

--- challenge_section.py
+++ challenge_section.py
@@ -1,11 +1,13 @@
 
 def two_list_dictionary(l1, l2):
-    if len(l2)==len(l1):
-        dict = {i: j for i in l1 for j in l2 if l1.index(i) == l2.index(j)}
-    if len(l2) >len(l1):
-        
+    dct = {}
+    for i in len(l1):
+        dct[l1[i]] = l2[i]
+    else:
+        dct[l1[i]] = None
 
-l1 = ['x', 123, 45, "things"]
+
+l1 = ['x', 123, 45, "things", "keys"]
 l2 = ['rt', "hello", 34, '45']
 
 print(two_list_dictionary(l1, l2))
