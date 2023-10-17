53b8c2f2c3e08de662edfbd5ca430791b4636b2f
Nasim <nasim.rjb@gmail.com>
2023-10-17 18:04:43 +0330

    Update challenge_section.py

--- challenge_section.py
+++ challenge_section.py
@@ -1,13 +1,36 @@
 
-def two_list_dictionary(l1, l2):
-    dct = {}
-    for i in len(l1):
-        dct[l1[i]] = l2[i]
-    else:
-        dct[l1[i]] = None
+# def two_list_dictionary(l1, l2):
+#     dct = {}
+#     for i, val in enumerate(l1):
+#         dct[l1[i]] = l2[i]
+#     else:
+#         dct[l1[i]] = None
+#     return dct
+# def two_list_dictionary(keys, values):
+#     collection = {}
+
+#     for idx, val in enumerate(keys):
+#         if idx < len(values):
+#             collection[keys[idx]] = values[idx]
+#         else:
+#             collection[keys[idx]] = None
+
+#     return collection
 
 
-l1 = ['x', 123, 45, "things", "keys"]
-l2 = ['rt', "hello", 34, '45']
+# l1 = ['x', 123, 45, "things", "keys"]
+# l2 = ['rt', "hello", 34, '45']
 
-print(two_list_dictionary(l1, l2))
+# print(two_list_dictionary(l1, l2))
+
+###########################################################
+# def range_in_list(lst, start, end):
+#     sum = 0
+#     for i, j in enumerate(lst, start):
+#         if i <= end:
+#             sum += lst[i]
+#     return sum
+
+
+# print(range_in_list([0, 1, 2, 3, 4, 5, 6, 7, 8], 3, 7))
+###########################################################
