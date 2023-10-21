dbe7735c2d7776f7d749f0dfa479e805386cdb2a
Nasim <nasim.rjb@gmail.com>
2023-10-21 19:46:02 +0330

    Create project-euler.py

---
+++ project-euler.py
@@ -0,0 +1,28 @@
+########## 1 ##########
+
+# def multiples(a):
+#     count = 0
+#     for i in range(a):
+#         if i % 3 == 0 or i % 5 == 0:
+#             count += i
+#     return count
+
+
+# print(multiples(1000))
+
+########## 2 ##########
+
+lst = [0]*400
+lst[0] = 1
+lst[1] = 2
+
+for i in range(0, 398):
+    lst[i+2] = lst[i+1]+lst[i]
+print(lst)
+sum = 0
+for j in lst:
+    if j < 4000000 and j % 2 == 0:
+        sum += j
+print(sum)
+
+########## 3 ##########
