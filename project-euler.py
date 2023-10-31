d4a2357f3b58b84e8f79731bb6765a83351d5767
Nasim <nasim.rjb@gmail.com>
2023-10-31 19:26:05 +0330

    Update project-euler.py

--- project-euler.py
+++ project-euler.py
@@ -41,17 +41,25 @@
 # print(largest_prime(600851475143))
 
 
-######################
-def largest_prime(n):
-    count = 0
-    lst = []
-    for i in range(n, 0, -1):
-        if n % i == 0:
+########## 4 ##########
+# lst = []
+# for i in range(999):
+#     for j in range(999):
+#         m = i*j
+#         n = str(m)
+#         if n[::-1] == n:
+#             lst.append(n)
+
+# lst = [int(i) for i in lst]
+# print(max(lst))
+
+########## 5 ##########
+count = 0
+for i in range(300000000, 0, -1):
+    for j in range(20, 0, -1):
+        if i % j == 0:
             count += 1
-        if count == 2:
-            lst.append(i)
+    if count == 20:
+        print(i)
     count = 0
-    return lst
-
-
-print(largest_prime(33))
+########## 6 ##########
