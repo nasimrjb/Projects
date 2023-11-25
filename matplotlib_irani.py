4c7dfaf07c36424fa83f8f5dcd52ec7625b435c6
Nasim <nasim.rjb@gmail.com>
2023-11-25 20:55:20 +0330

    س

---
+++ matplotlib_irani.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(
+    'D://Courses//Data Science with Python//1-python datasets//banking.txt')
+print(data.head(10))
+print(data.columns)
