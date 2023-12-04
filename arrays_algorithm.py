d76238a854f7d9641de1170ec6b2774c4e00f79b
Nasim <nasim.rjb@gmail.com>
2023-12-04 14:16:47 +0330

    Update arrays_algorithm.py

--- arrays_algorithm.py
+++ arrays_algorithm.py
@@ -1,10 +1,17 @@
-array = ['a', 'b', 'c', 'd', 'e']
+# array = ['a', 'b', 'c', 'd', 'e']
 
 
-def returnpairs(array):
-    for i in range(len(array)):
-        for j in range(len(array)):
-            print(array[i], array[j])
+# def returnpairs(array):
+#     for i in range(len(array)):
+#         for j in range(len(array)):
+#             print(array[i], array[j])
 
 
-returnpairs(array)
+# returnpairs(array)
+
+Array1 = ['a', 'b', 'c', 'x']
+Array2 = ['d', 'e', 'f', 'x']
+
+for item in Array2:
+    if item in Array1:
+        
