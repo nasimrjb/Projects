9f07b90bf57610c7e023185b205117aa9efd9010
Nasim <nasim.rjb@gmail.com>
2024-01-18 15:00:12 +0330

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 import numpy as np
+import pandas as pd
+import scipy as scipy
 
-x = [[1,2,3],[4,5,6],[7,8,9]]
+x = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 x = np.array(x)
-print(x[3::1,-1])
+# print(x[3::1, -1])
 
-tarr = np.random.randint(0,10,(4,3))
-print(tarr)
+tarr = np.random.randint(0, 10, (4, 3))
+# print(tarr)
+
+
+numbers = [2, 2.1, 3]
+p_numbers = pd.Series(numbers)
+# print(p_numbers)
+
+animals = {
+    'n1': 'Tiger',
+    'n2': 'Bear',
+    'n3': None
+}
+p_animals = pd.Series(animals)
+# print(p_animals)
+
+# print(p_animals.iloc[2])
